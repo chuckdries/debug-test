88ffa6dce62b5235ce64505f621ff7ca4fd8882a
Chuck Dries <chuck@chuckdries.com>
2018-03-16 13:46:19 -0700

    Anonymous function bp

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 
 console.log('hello');
-app.get('/', function(req, res){
-    var r = req;
-    res.send('asdf');
-});
-app.listen(8000);
 
+const foo = (req, res) => {
+    console.log('fooing');
+    res.send('bar');
+}
+
+app.get('/', foo);
+app.listen(8000);
